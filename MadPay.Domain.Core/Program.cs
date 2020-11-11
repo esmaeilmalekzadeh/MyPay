ea9651de3aa7cf5bb49e62c1b4ae999ed8a3cf71
esmaeilmalekzadeh <esmaeil.malekzadeh.s@gmail.com>
2020-11-11 17:55:19 +0330

    Init Projects

---
+++ MadPay.Domain.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MadPay.Domain.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
